112bf880fa00b34a716e22917add990ab9287b94
zhangziqi1988 <zhangziqi1988@126.com>
2018-07-12 21:32:46 +0800

    添加练习7

--- machine-learning-ex6/ex6/ex6_spam.m
+++ machine-learning-ex6/ex6/ex6_spam.m
@@ -167,3 +167,13 @@ p = svmPredict(model, x);
 
 fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
 fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
+
+filename = 'myspam1.txt';
+% Read and predict
+file_contents = readFile(filename);
+word_indices  = processEmail(file_contents);
+x             = emailFeatures(word_indices);
+p = svmPredict(model, x);
+
+fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
+fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
